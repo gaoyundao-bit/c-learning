5f44671830070f357b120133913b3badfb577109
gaoyundao-bit <16605198688@163.com>
2025-07-30 15:05:42 +0800

    try in vscode

--- function.c
+++ function.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
-int Add(int x,int y)
+int Add(int m,int n)
 {
     int z = 0;
-    z = x + y;
+    z = m + n;
     return z;
+}
+char ch1[] = "hello world1";
+char ch2[] = "hello world";
+int compare(char ch1[], char ch2[]){
+    int i = 0;
+    while (ch1[i]==ch2[i]){
+        if(ch1[i] == '\0' ) {
+            return 0;
+        }
+        i++;
+    }
+    return ch1[i]> ch2[i] ? 1 : -1;
 }
 int main()
 {
@@ -11,6 +23,16 @@ int main()
     int b = 0;
     scanf("%d%d",&a,&b);
     int sum = Add(a,b);
-    printf("����%d\n", sum);
+    printf("和为%d\n", sum);
+    int result = compare(ch1, ch2);
+    if (result == 0) {
+        printf("两个字符串相等\n");
+    }
+    else if (result > 0) {
+        printf("第一个字符串大于第二个字符串\n");
+    }
+    else {
+        printf("第一个字符串小于第二个字符串\n");
+    }
     return 0;
 }
