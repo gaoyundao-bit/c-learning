355b0051b7df7d8517a4da74d2ec542945d60ce8
gaoyundao-bit <16605198688@163.com>
2025-08-02 16:20:06 +0800

    202508021619

---
+++ zhizhen7.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int* p;
+    *p = 84848484;
+
+    printf("%s",p);
+
+}
---
+++ zhizhen8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    int* arr[] = {&a,&b,&c};
+    printf("%d\n",*arr[0]);
+}
