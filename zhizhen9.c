c590e1358e8bdc360cd9786bfe0cbe986a7a9001
gaoyundao-bit <16605198688@163.com>
2025-08-03 14:22:19 +0800

    202508031422

--- zhizhen9.c
+++ zhizhen9.c
@@ -4,8 +4,12 @@ int main () {
     int b[] ={4,5,6};
     int c[] ={7,8,9};
     int* arr[] = {a,b,c};
-    //printf("%d\n",arr[0][2]) ;
-     for(int i = 0;i<3;i++){
-        for(int j = 0 ;j < 3;
-     }
+    
+    for(int i = 0;i<3;i++){
+        for(int j = 0;j<3;j++){
+            printf("%d ",*(arr[i]+j));
+        }
+        printf("\n");
+    }
+    return 0;
 }
